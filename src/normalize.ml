abfc1d90f0468db418f26714ebb103ef92e6563e
DadaIsCrazy <darius.mercadier@gmail.com>
2017-03-31 16:33:53 +0200

    Turn optimizer on

--- src/normalize.ml
+++ src/normalize.ml
@@ -60,7 +60,7 @@ let norm_prog (prog: prog)  =
     if Bitsliceable.bitsliceable perm_expanded then
       (let normed = Norm_bitslice.norm_prog perm_expanded in
       print "PRE-NORMALIZED:" normed;
-      let inlined = (*Inline.inline*) normed in
+      let inlined = Inline.inline normed in
       print "INLINED:" inlined;
       Norm_bitslice.norm_prog inlined)
     else
