3ae11a5b4079d40ce4b447846d88ac927187bca1
DadaIsCrazy <darius.mercadier@gmail.com>
2017-05-26 18:07:29 +0200

    Update reg alloc

--- src/reg_alloc.ml
+++ src/reg_alloc.ml
@@ -1,12 +1,12 @@
 open Usuba_AST
 open Utils
 
-let update_hoh hash v1 v2 =
-  match env_fetch hash v1 with
-  | Some h -> Hashtbl.replace h v2 1
+let update_hoh hash k1 k2 =
+  match env_fetch hash k1 with
+  | Some h -> Hashtbl.replace h k2 1
   | None   -> let h = Hashtbl.create 60 in
-              Hashtbl.add h v2 1;
-              Hashtbl.add hash v1 h
+              Hashtbl.add h k2 1;
+              Hashtbl.add hash k1 h
        
 let build_inter (deqs:deq list) (p_in:p) (p_out:p)
     : (var, (var,int) Hashtbl.t) Hashtbl.t  =
@@ -16,6 +16,8 @@ let build_inter (deqs:deq list) (p_in:p) (p_out:p)
 
   (* Marking the out variables as alive *)
   List.iter (fun (id,_,_) -> Hashtbl.add curr (Var id) 1) p_out;
+
+  print_endline "Around here";
 
   (* Computing live var at any point *)
   List.iteri (fun i deq -> match deq with
@@ -28,16 +30,23 @@ let build_inter (deqs:deq list) (p_in:p) (p_out:p)
              | _ -> unreached ()
              ) (List.rev deqs);
 
+  print_endline "And now here";
+  
   (* Computing the adjacency list of each variable *)
   let graph = Hashtbl.create 100 in
   Array.iter (fun l -> List.iter (fun x ->
                                   List.iter (fun y ->
                                              if y <> x then
                                                update_hoh graph x y) l) l) live;
+
+  print_endline "And I'm done";
+  
   graph
        
 let alloc_deqs def (deqs:deq list) : deq list =
-  let _ = build_inter deqs def.p_in def.p_out in
+  (* Get interference graph *)
+  let graph = build_inter deqs def.p_in def.p_out in
+  
   deqs 
 
 let alloc_def (def:def) : def =
@@ -48,4 +57,3 @@ let alloc_def (def:def) : def =
 let alloc_reg (prog:prog) : prog =
    { nodes = List.map alloc_def prog.nodes }
   
-     
