cc2a7b72cf98382bffd9365c5339ff7c107f004d
DadaIsCrazy <darius.mercadier@gmail.com>
2017-11-03 15:57:32 +0100

    Update inlining

--- src/optimization/inline.ml
+++ src/optimization/inline.ml
@@ -1,228 +1,150 @@
 open Usuba_AST
 open Utils
-
-let max_pressure = 14
-
-let get_body (node:def) : deq list =
-  match node.node with
-  | Single(_,body) -> body
-  | _ -> raise (Error "Not a Single")
        
-let reg_pressure (deqs: deq list) (p_out:p) : int =
-  let live : (var,bool) Hashtbl.t = Hashtbl.create 100 in (* variables currently alive *)
-  
-  let p_out = List.map (fun ((x,_),_) -> Var x) p_out in
-  
-  List.iter (fun x -> Hashtbl.add live x true) p_out;
-  let reg_pressure = ref (Hashtbl.length live) in
-
-  List.iter (function
-              | Norec(l,e) ->
-                 List.iter (fun x -> Hashtbl.replace live x true) (get_used_vars e);
-                 List.iter (fun x -> Hashtbl.remove live x) l;
-                 reg_pressure := max !reg_pressure (Hashtbl.length live)
-              | _ -> unreached()) (List.rev deqs);
-  
-  !reg_pressure
-       
-let rec has_no_inner_deps (vars:p) (body:deq list) : bool =
-  let env = Hashtbl.create 100 in
-  List.iter (fun ((id,_),_) -> env_add env id true) vars;
-  not (List.exists (function
-                     | Norec(_,e) -> List.exists
-                                       (fun x ->
-                                        match env_fetch env (get_var_name x) with
-                                        | Some _ -> true
-                                        | None -> false) (get_used_vars e)
-                     | _ -> false) body)
-       
-let should_inline (dest:def) (def:def) : bool =
-  match def.node with
-  | Single(vars,body) ->
-     if has_no_inner_deps vars body then true
-     else
-       let pressure_dst = reg_pressure (get_body dest) dest.p_out in
-       let pressure_def = reg_pressure (get_body def) def.p_out in
-       pressure_dst + pressure_def <= max_pressure
-  | _ -> false
-           
-let is_call_free (def:def) : bool =
-  match def.node with
-  | Single(_,body) ->
-     List.fold_left (&&) true
-                    (List.map (function
-                                | Norec(_, Fun _) | Norec(_, Fun_v _) -> false
-                                | _ -> true) body)
-  | _ -> false
-
-let known_calls (env:(def*p*deq list) env) (deqs:deq list) : bool =
-  List.fold_left (&&) true
-                 (List.map (function
-                             | Norec(_,Fun(f,_))
-                             | Norec(_,Fun_v(f,_,_))-> (match env_fetch env f with
-                                                     | Some _ -> true
-                                                     | None -> false)
-                             | _ -> true) deqs)
-                    
-let rec get_next_node (env:(def*p*deq list) env)
-                      (todo:(def*p*deq list) env) :
-  def*p*deq list =
-  let next = ref "" in
-  Hashtbl.iter (fun x (_,_,y) -> if known_calls env y then
-                                     next := x) todo;
-  let ret = Hashtbl.find todo !next in
-  Hashtbl.remove todo !next;
-  ret
-
-let rec rename_var pref (var:var) : var =
-  match var with
-  | Var id -> Var (fresh_prefix pref id)
-  | Field(v,e) -> Field(rename_var pref v,e)
-  | Index(id,e) -> Index(fresh_prefix pref id,e)
-  | Range(id,ei,ef) -> Range(fresh_prefix pref id,ei,ef)
-  | Slice(id,l) -> Slice(fresh_prefix pref id,l)
-                            
-let rec rename_expr pref (e:expr) : expr =
+(* Convert variables names inside an expression *)
+let rec update_expr var_env expr_env (e:expr) : expr =
+  let rec_call = update_expr var_env expr_env in
   match e with
   | Const _ -> e
-  | ExpVar(v) -> ExpVar (rename_var pref v)
-  | Tuple l -> Tuple (List.map (rename_expr pref) l)
-  | Not e -> Not (rename_expr pref e)
-  | Shift(op,e,n) -> Shift(op,rename_expr pref e,n)
-  | Log(op,x,y) -> Log(op,rename_expr pref x,rename_expr pref y)
-  | Arith(op,x,y) -> Arith(op,rename_expr pref x,rename_expr pref y)
-  | Fun(id,l) -> Fun(id,List.map (rename_expr pref) l)
-  | Fun_v(id,n,l) -> Fun_v(id,n,List.map (rename_expr pref) l)
-  | _ -> e
+  | ExpVar v -> ( match Hashtbl.find_opt expr_env v with
+                  | Some e -> e
+                  | None -> ExpVar (Hashtbl.find var_env v) )
+  | Not e -> Not (rec_call e)
+  | Log(op,x,y) -> Log(op,rec_call x,rec_call y)
+  | Fun(f,l) -> Fun(f,List.map rec_call l)
+  | _ -> assert false
+
+(* Convert the variable names *)
+let update_vars (var_env : (var,var) Hashtbl.t)
+                (expr_env: (var,expr) Hashtbl.t)
+                (body:deq list) : deq list =
+  List.map (function
+      | Rec _ -> assert false
+      | Norec(lhs,e) -> Norec( List.map (fun id -> Hashtbl.find var_env id) lhs,
+                               update_expr var_env expr_env e )) body
+
+
+(* Inline a specific call (defined by lhs & args) *)
+let inline_call (to_inl:def) (args:expr list) (lhs:var list) (cpt:int) :
+      p * deq list =
+  (* Define a name conversion function *)
+  let conv_name (id:ident) : ident =
+    { id with name = Printf.sprintf "%s_%d_%s" to_inl.id.name cpt id.name } in
+  
+  (* Extract body, vars, params and name of the node to inline *)
+  let (vars_inl,body_inl) = match to_inl.node with
+    | Single(vars,body) -> vars, body
+    | _ -> assert false in
+  let p_in  = to_inl.p_in  in
+  let p_out = to_inl.p_out in
+
+  (* alpha-conversion environment *)
+  let var_env = Hashtbl.create 100 in
+  let expr_env = Hashtbl.create 100 in
+  (* p_out replaced by the lhs *)
+  List.iter2 ( fun ((id,_),_) v -> Hashtbl.add var_env (Var id) v) p_out lhs;
+  (* p_in replaced by the expressions of arguments *)
+  List.iter2 ( fun ((id,_),_) e -> Hashtbl.add expr_env (Var id) e) p_in args;
+  (* Create a list containing the new variables names *)
+  let vars = List.map (fun ((id,typ),ck) -> ((conv_name id,typ),ck)) vars_inl in
+  (* nodes variables alpha-converted *)
+  List.iter2 ( fun ((id,_),_) ((id',_),_) -> Hashtbl.add var_env (Var id)
+                                               (Var id')) vars_inl vars;
+
+  vars, update_vars var_env expr_env body_inl  
+  
+  
+(* Inline all the calls to "to_inl" in a given node 
+   (desribed by its variables and body "vars,body") *)
+let inline_in_node ((vars,body):p*deq list) (to_inl:def) : p * deq list =
+  let f_inl = to_inl.id.name in
+  (* maintain a counter for variables alpha-conversion *)
+  let cpt   = ref 0 in
+  
+  List.fold_left
+    (* Unpacked the list bellow into a single list of vars and 
+       a list of deqs *)
+    (fun (vars,body) (v,b) -> (v @ vars, b @ body))
+    ([],[])
+    (* Find the calls to f_inl, and inline them. 
+       This will introduce new variables, which is 
+       why maps returns a (p * deq list) list. *)
+    ( List.map (
+          fun eqn -> match eqn with
+          | Rec _ -> assert false
+          | Norec(lhs,e) -> match e with
+                            | Fun(f,l) when f.name = f_inl ->
+                               incr cpt;
+                               inline_call to_inl l lhs !cpt
+                            | _ -> [], [eqn]
+        ) body )
     
-let rec rename_deqs pref (deqs:deq list) : deq list =
-  List.map (fun x -> match x with
-                     | Norec(p,e) ->
-                        Norec(List.map (rename_var pref) p,rename_expr pref e)
-                     | Rec(id,ei,ef,d) ->
-                        Rec(id,ei,ef,rename_deqs pref d)) deqs
-           
-let inline_deqs env (node:def) (deqs: deq list) (conf:config): p*deq list =
-  let cpt = ref 0 in
-  let add_vars  = ref [] in
-  let body =
-    List.flatten @@
-      List.map
-        (fun x ->
-         match x with
-         | Norec (pat,e) ->
-            ( match e with
-              | Fun(f,l) ->
-                 let (def,vars,body) = Hashtbl.find env f.name in
-                 let p_in = def.p_in in
-                 let p_out = def.p_out in
 
-                 if (not (is_noinline def)) ||  conf.inline_all
-                   (* && (should_inline node def) *)then
-                   begin
-                     let pref = def.id.name ^ ((string_of_int !cpt) ^ "_") in
-                     incr cpt;
-                     
-                     add_vars :=
-                       !add_vars @
-                         (List.map (fun ((id,typ),ck) -> (fresh_prefix pref id,typ),ck) p_in) @
-                           (List.map (fun ((id,typ),ck) -> (fresh_prefix pref id,typ),ck) p_out) @
-                             (List.map (fun ((id,typ),ck) -> (fresh_prefix pref id,typ),ck) vars);
-                     
-                     let pat_in  = List.map (fun ((id,typ),ck) -> Var(fresh_prefix pref id)) p_in in
-                     let pat_out = Tuple(List.map (fun ((id,typ),ck) ->
-                                                   ExpVar(Var (fresh_prefix pref id))) p_out) in
-                     
-                     let body = rename_deqs pref body in
-                     
-                     (Norec(pat_in,Tuple(l))) :: body @ [ Norec(pat,pat_out) ]
-                   end
-                 else
-                   [ Norec (pat,e) ]
-              | _ -> [Norec (pat,e)])
-         | Rec _ -> raise (Not_implemented "inline Rec")) deqs in
-  !add_vars,body
+(* Perform the inlining of node "to_inline" at every call point *)
+let do_inline (prog:prog) (to_inline:def) : prog =
 
-
-(* (\* Returns true if the node can be inlined now. ie: *)
-(*     - is not already inlined *)
-(*     - it doesn't have the attribute "no_inline" *)
-(*     - it doesn't contain any function call, or *)
-(*     - every function call it contains is to a node "no_inline" *\) *)
-(* let can_inline env (node:def) : bool = *)
-(*   (\* Is not already inlined *\) *)
-(*   (not (Hashtbl.find env node.id.name)) && *)
-(*     (\* Is not "no_inline" *\) *)
-(*     (not is_noinline node) && *)
-(*       (\* Doesn't contain any call, or calls to "no_inline" *\) *)
-(*       (is_call_free env node) *)
+  { nodes = List.map (fun def ->
+                match def.node with
+                | Single(vars,body) ->
+                   let (vars,body) = inline_in_node (vars,body) to_inline in
+                   { def with node = Single(vars,body) }
+                | _ -> def) prog.nodes }
+  
+  
+(* Returns true if def doesn't contain any function call,
+   or if those calls are to functions that are not going 
+   to be inlined *)
+let is_call_free env (def:def) : bool =
+  match def.node with
+  | Single(_,body) ->
+     List.for_all (function
+         | Rec _ -> assert false
+         | Norec(_,e) -> match e with
+                         | Fun(f,_) ->
+                            is_noinline (Hashtbl.find env f.name)
+                         | _ -> true) body
+  | _ -> false
+  
+(* Returns true if the node can be inlined now. ie:
+    - is not already inlined
+    - it doesn't have the attribute "no_inline"
+    - it doesn't contain any function call, or
+    - every function call it contains is to a node "no_inline" *)
+let can_inline env inlined conf (node:def) : bool =
+  (* Is not already inlined *)
+  (not (Hashtbl.find inlined node.id.name)) &&
+    (* Is not "no_inline" or inlining is forced *)
+    ( (not (is_noinline node)) || conf.inline_all ) &&
+      (* Doesn't contain any call, or calls to "no_inline" *)
+      (is_call_free env node)
 
       
-(* let rec _inline (prog:prog) (conf:config) inlined : prog = *)
+let rec _inline (prog:prog) (conf:config) inlined : prog =
   
-(*   let env = Hashtbl.create 20 in *)
-(*   List.iter (fun x -> Hashtbl.add env x.ident.name x) prog.nodes; *)
+  let env = Hashtbl.create 20 in
+  List.iter (fun x -> Hashtbl.add env x.id.name x) prog.nodes;
 
-(*   (\* If there is a node that can be inlined *\) *)
-(*   if List.exists (can_inline env inlined) prog.nodes then *)
-(*     (\* find the node to inline *\) *)
-(*     let to_inline = List.find (can_inline env) prog.nodes in *)
-(*     (\* inline it *\) *)
-(*     let prog' = do_inline env prog to_inline in *)
-(*     (\* add it to the hash of inlined nodes *\) *)
-(*     Hashtbl.replace inlined to_inline.id.name true; *)
-(*     (\* continue inlining *\) *)
-(*     _inline prog' conf inlined *)
-(*   else *)
-(*     (\* inlining is done, return the prog *\) *)
-(*     prog *)
-
-(* let inline (prog:prog) (conf:config) : prog = *)
-(*   (\* Hashtbl containing the inlining status of each node: *)
-(*      false if it is not already inlined, true if it is *\) *)
-(*   let inlined = Hashtbl.create 20 in *)
-(*   List.iter (fun x -> Hashtbl.add inlined x.id.name false) prog.nodes; *)
-(*   (\* The last node is the entry point, it wouldn't make sense to try inline it *\) *)
-(*   Hashtbl.replace inlined (last prog.nodes).id.name true; *)
-
-(*   (\* And now, perform the inlining *\) *)
-(*   _inline prog conf inlined *)
-  
+  (* If there is a node that can be inlined *)
+  if List.exists (can_inline env inlined conf) prog.nodes then
+    (* find the node to inline *)
+    let to_inline = List.find (can_inline env inlined conf) prog.nodes in
+    (* inline it *)
+    let prog' = do_inline prog to_inline in
+    (* add it to the hash of inlined nodes *)
+    Hashtbl.replace inlined to_inline.id.name true;
+    (* continue inlining *)
+    _inline prog' conf inlined
+  else
+    (* inlining is done, return the prog *)
+    prog
 
 let inline (prog:prog) (conf:config) : prog =
-  
-  let env  = Hashtbl.create 20 in
-  List.iter (fun x -> match x.node with
-                      | Single(vars,body) ->
-                         env_add env x.id (x,vars,body)
-                      | _ -> ())
-            (List.filter is_call_free prog.nodes);
-  let todo = Hashtbl.create 20 in
-  List.iter (fun x -> match x.node with
-              | Single(vars,body) ->
-                 env_add todo x.id (x,vars,body)
-              | _ -> ())
-            (List.filter (fun x -> not @@ is_call_free x) prog.nodes);
-  while Hashtbl.length todo <> 0  do
-    let (node,vars,body) = get_next_node env todo in
-    let (vars',body') = inline_deqs env node body conf in
-    let new_vars =  vars @ vars' in
-    env_add env node.id ({ node with node = Single(new_vars,body') } ,new_vars,body')
-  done;
-  let prog = List.map (fun x ->
-                       match x.node with
-                       | Single _ -> (
-                         match env_fetch env x.id with
-                         | Some (node,vars,body) -> node
-                         | None -> raise (Error ("Not found: " ^ x.id.name)))
-                       | _ -> x)
-                      prog.nodes in
-  let no_inlined = List.filter (fun x -> List.exists (function
-                                                | No_inline -> true
-                                                | Inline    -> false) x.opt) prog in
-  let main = List.nth prog (List.length prog - 1) in
-  if List.exists (fun x -> x = main) no_inlined then
-    { nodes = no_inlined }
-  else
-    { nodes = no_inlined @ [ main ] }
-                      
+  (* Hashtbl containing the inlining status of each node:
+     false if it is not already inlined, true if it is *)
+  let inlined = Hashtbl.create 20 in
+  List.iter (fun x -> Hashtbl.add inlined x.id.name false) prog.nodes;
+  (* The last node is the entry point, it wouldn't make sense to try inline it *)
+  Hashtbl.replace inlined (last prog.nodes).id.name true;
+
+  (* And now, perform the inlining *)
+  _inline prog conf inlined
