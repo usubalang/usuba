613d456feb17057f4df33b08ea30cbda31f659e5
DadaIsCrazy <darius.mercadier@gmail.com>
2018-06-14 15:01:51 +0200

    Fix array expansion bug with main
    The parameters of the main are now not expanded no matter what.
    (we never want our main to have 200 parameters: arrays are always
    better).
    It used to be dealt with at the time of generating the C code, but
    it was a bit hacky and make further optimizations (interleaving)
    complicated.

--- src/normalization/expand_array.ml
+++ src/normalization/expand_array.ml
@@ -35,6 +35,14 @@ let rec uses_var (e:arith_expr) : bool =
   | Var_e _ -> true
   | Op_e(_,e1,e2) -> (uses_var e1) && (uses_var e2)
 
+let rec need_to_keep env_keep (v:var) : bool =
+  match v with
+  | Var id -> begin match Hashtbl.find_opt env_keep id with
+                    | Some _ -> true
+                    | _ -> false end
+  | Index(v,_) -> need_to_keep env_keep v
+  | _ -> assert false                       
+                                        
 let rec remove_arr (v:var) : var =
   match v with
   | Var _ -> v
@@ -45,38 +53,46 @@ let rec remove_arr (v:var) : var =
        | _ -> assert false
      end
   | _ -> assert false
-                                        
-let rec expand_var env_var env force (v:var) : var list =
+(* Warning: this shadows (and calls) the definition above *)
+let remove_arr env_keep (v:var) : var =
+  if need_to_keep env_keep v then v
+  else remove_arr v
+
+                
+let rec expand_var env_var env_keep env force (v:var) : var list =
   let rec aux (v:var) : var list = 
     match v with
     | Var _ -> [ v ]
     | Index(v',i) -> List.map (fun x -> Index(x,simpl_arith env i)) (aux v')
-    | Range(v',ei,ef) -> (try
-                             let ei = eval_arith env ei in
-                             let ef = eval_arith env ef in
-                             flat_map (fun i -> aux (Index(v',Const_e i)))
-                                      (gen_list_bounds ei ef)
-                                      (* The Not_found can be raised by the calls to eval_arith *)
-                           with Not_found -> raise Need_unroll)
+    | Range(v',ei,ef) ->
+       (try
+           let ei = eval_arith env ei in
+           let ef = eval_arith env ef in
+           flat_map (fun i -> aux (Index(v',Const_e i)))
+                    (gen_list_bounds ei ef)
+                    (* Not_found can be raised by the calls to eval_arith *)
+         with Not_found -> raise Need_unroll)
     | Slice(v',el) -> flat_map (fun i -> aux (Index(v',i))) el in
   if force then
-    List.map remove_arr (flat_map (Utils.expand_var env_var) (aux v))
+    List.map (remove_arr env_keep) (flat_map (Utils.expand_var env_var) (aux v))
   else
     aux v
                              
-let expand_vars env_var env force (vars:var list) : var list =
-  flat_map (expand_var env_var env force) vars
+let expand_vars env_var env_keep env force (vars:var list) : var list =
+  flat_map (expand_var env_var env_keep env force) vars
            
-let rec expand_expr env_var env force (e:expr) : expr =
-  let rec_call = expand_expr env_var env force in
+let rec expand_expr env_var env_keep env force (e:expr) : expr =
+  let rec_call = expand_expr env_var env_keep env force in
   match e with
   | Const _ -> e
-  | ExpVar v -> Tuple(List.map (fun x -> ExpVar x) (expand_var env_var env force v))
+  | ExpVar v -> Tuple(List.map (fun x -> ExpVar x)
+                               (expand_var env_var env_keep env force v))
   | Tuple el -> Tuple(List.map rec_call el)
   | Not e' -> Not (rec_call e')
   | Shift(op,e1,ae) -> Shift(op,rec_call e1,ae)
   | Log(op,e1,e2) -> Log(op,rec_call e1,rec_call e2)
-  | Shuffle(v,pat) -> Tuple(List.map (fun x -> Shuffle(x,pat)) (expand_var env_var env force v))
+  | Shuffle(v,pat) -> Tuple(List.map (fun x -> Shuffle(x,pat))
+                                     (expand_var env_var env_keep env force v))
   | Arith(op,e1,e2) -> Arith(op,rec_call e1,rec_call e2)
   | Fun(f,el) -> Fun(f,List.map rec_call el)
   | Fun_v(f,ae,el) -> (try Fun(fresh_suffix f (sprintf "%d'" (eval_arith env ae)),
@@ -85,29 +101,30 @@ let rec expand_expr env_var env force (e:expr) : expr =
   | Fby _ | When _ | Merge _ -> e
 
 
-let rec do_unroll env_var env force x ei ef deqs : deq list =
+let rec do_unroll env_var env_keep env force x ei ef deqs : deq list =
   try
     let ei = eval_arith env ei in
     let ef = eval_arith env ef in
     let eqs = flat_map (fun i -> env_update env x i;
-                                 expand_deqs env_var ~env:env force deqs)
+                                 expand_deqs env_var env_keep ~env:env force deqs)
                        (gen_list_bounds ei ef) in
     env_remove env x;
     eqs
   with Not_found -> raise Need_unroll
 
-and expand_deqs env_var ?(env=make_env ()) (force:bool) (deqs:deq list) : deq list =
+and expand_deqs env_var env_keep ?(env=make_env ()) (force:bool) (deqs:deq list) : deq list =
   flat_map
     (fun deq -> 
      match deq with
-     | Norec(lhs,e) -> [ Norec(expand_vars env_var env force lhs, expand_expr env_var env force e) ]
+     | Norec(lhs,e) -> [ Norec(expand_vars env_var env_keep env force lhs,
+                               expand_expr env_var env_keep env force e) ]
      | Rec(x,ei,ef,deqs,opts) ->
         if List.mem Unroll opts || force then
-          do_unroll env_var env force x ei ef deqs
+          do_unroll env_var env_keep env force x ei ef deqs
         else
           try
-            [ Rec(x,ei,ef,expand_deqs env_var ~env:env force deqs,opts) ]
-          with Need_unroll -> do_unroll env_var env force x ei ef deqs)
+            [ Rec(x,ei,ef,expand_deqs env_var env_keep ~env:env force deqs,opts) ]
+          with Need_unroll -> do_unroll env_var env_keep env force x ei ef deqs)
     deqs
     
 
@@ -126,19 +143,40 @@ let expand_p (p:p) : p =
   flat_map aux p
 
            
-let expand_def (force:bool) (def:def) : def =
+let build_env_keep (p_in:p) (p_out:p) =
+  let env = Hashtbl.create 100 in
+  
+  let f ((id,typ),_) =
+    Hashtbl.add env id true in
+
+  List.iter f p_in;
+  List.iter f p_out;
+
+  env
+           
+           
+let expand_def (force:bool) (keep_param:bool) (def:def) : def =
   let expand_p = if force then expand_p else (fun x -> x) in
-  { def with p_in  = expand_p def.p_in;
-             p_out = expand_p def.p_out;
+  { def with p_in  = if keep_param then def.p_in  else expand_p def.p_in;
+             p_out = if keep_param then def.p_out else expand_p def.p_out;
              node  = match def.node with
                      | Single(vars,body) ->
-                        let env_var = build_env_var def.p_in def.p_out vars in
-                        Single(expand_p vars, expand_deqs env_var force body)
+                        let env_var  = build_env_var def.p_in def.p_out vars in
+                        let env_keep = if keep_param then build_env_keep def.p_in def.p_out
+                                       else Hashtbl.create 100 in
+                        Single(expand_p vars, expand_deqs env_var env_keep force body)
                      | _ -> def.node }
-    
+
+
+(* Like `map f l` but `g` is applied to the last element instead of `f`. *)
+let rec map_special_last f g l =
+  match l with
+  | [] -> []
+  | [ x ] -> [ g x ]
+  | hd :: tl -> (f hd) :: (map_special_last f g tl)
 
     
 let rec expand_array (prog:prog) (conf:config): prog =
   let force = conf.no_arr || must_expand prog in
-  { nodes = List.map (expand_def force) prog.nodes }
+  { nodes = map_special_last (expand_def force false) (expand_def force true) prog.nodes }
 
