064184018fb53c03d1c1427a496a9c47e3f60057
DadaIsCrazy <darius.mercadier@gmail.com>
2018-11-11 20:52:48 +0100

    Add Rectangle cipher

---
+++ ciphers/rectangle_c/stream.h
@@ -0,0 +1,11 @@
+int crypto_stream_ecb( unsigned char *out,
+                       unsigned char *in,
+                       unsigned long long inlen,
+                       unsigned char *k
+                       );
+
+void ortho_speed ( unsigned char *out,
+                   unsigned char *in,
+                   unsigned long long inlen,
+                   unsigned char *k
+                   );
