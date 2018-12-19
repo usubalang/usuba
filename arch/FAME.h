01cbcf71ac551b466cf2f5ed45f0895ccb8e6367
DadaIsCrazy <darius.mercadier@gmail.com>
2018-12-19 17:10:08 +0100

    Fix missing parenthesis in FAME macros

--- arch/FAME.h
+++ arch/FAME.h
@@ -12,36 +12,36 @@
 
 #define RED(rd,y,i,a) {                                                 \
     if (i == 0b010) {                                                   \
-      rd = (a << 16) | (a & 0xFFFF);                                    \
-      y  = (a & 0xFFFF0000) | (a >> 16);                                \
+      rd = ((a) << 16) | ((a) & 0xFFFF);                                \
+      y  = ((a) & 0xFFFF0000) | ((a) >> 16);                            \
     }                                                                   \
     else if (i == 0b011) {                                              \
-      rd = (~a << 16) | (a & 0xFFFF);                                   \
-      y  = (~a & 0xFFFF0000) | (a >> 16);                               \
+      rd = (~(a) << 16) | ((a) & 0xFFFF);                               \
+      y  = (~(a) & 0xFFFF0000) | ((a) >> 16);                           \
     }                                                                   \
     else if (i == 0b100) {                                              \
-      rd = (a & 0xFF) | ((a & 0xFF) << 8) |                             \
-        ((a & 0xFF) << 16) | ((a & 0xFF) << 24);                        \
-      y  = ((a & 0xFF00) >> 8) | (a & 0xFF00) |                         \
-        ((a & 0xFF00) << 8) | ((a & 0xFF00) << 16);                     \
+      rd = ((a) & 0xFF) | (((a) & 0xFF) << 8) |                         \
+      (((a) & 0xFF) << 16) | (((a) & 0xFF) << 24);                      \
+      y  = (((a) & 0xFF00) >> 8) | ((a) & 0xFF00) |                     \
+      (((a) & 0xFF00) << 8) | (((a) & 0xFF00) << 16);                   \
     }                                                                   \
     else if (i == 0b101) {                                              \
-      rd = (a & 0xFF) | ((~a & 0xFF) << 8) |                            \
-        ((a & 0xFF) << 16) | ((~a & 0xFF) << 24);                       \
-      y  = ((a & 0xFF00) >> 8) | (~a & 0xFF00) |                        \
-        ((a & 0xFF00) << 8) | ((~a & 0xFF00) << 16);                    \
+      rd = ((a) & 0xFF) | ((~(a) & 0xFF) << 8) |                        \
+      (((a) & 0xFF) << 16) | ((~(a) & 0xFF) << 24);                     \
+      y  = (((a) & 0xFF00) >> 8) | (~(a) & 0xFF00) |                    \
+      (((a) & 0xFF00) << 8) | ((~(a) & 0xFF00) << 16);                  \
     }                                                                   \
     else if (i == 0b110) {                                              \
-      rd = ((a & 0xFF0000) >> 16) | ((a & 0xFF0000) >> 8) |             \
-        (a & 0xFF0000) | ((a & 0xFF0000) << 8);                         \
-      y  = ((a & 0xFF000000) >> 24) | ((a & 0xFF000000) >> 16) |        \
-        ((a & 0xFF000000) >> 8) | (a & 0xFF000000);                     \
+      rd = (((a) & 0xFF0000) >> 16) | (((a) & 0xFF0000) >> 8) |         \
+      ((a) & 0xFF0000) | (((a) & 0xFF0000) << 8);                       \
+      y  = (((a) & 0xFF000000) >> 24) | (((a) & 0xFF000000) >> 16) |    \
+      (((a) & 0xFF000000) >> 8) | ((a) & 0xFF000000);                   \
     }                                                                   \
     else if (i == 0b111) {                                              \
-      rd = ((a & 0xFF0000) >> 16) | ((~a & 0xFF0000) >> 8) |            \
-        (a & 0xFF0000) | ((~a & 0xFF0000) << 8);                        \
-      y  = ((a & 0xFF000000) >> 24) | ((~a & 0xFF000000) >> 16) |       \
-        ((a & 0xFF000000) >> 8) | (~a & 0xFF000000);                    \
+      rd = (((a) & 0xFF0000) >> 16) | ((~(a) & 0xFF0000) >> 8) |        \
+      ((a) & 0xFF0000) | ((~(a) & 0xFF0000) << 8);                      \
+      y  = (((a) & 0xFF000000) >> 24) | ((~(a) & 0xFF000000) >> 16) |   \
+      (((a) & 0xFF000000) >> 8) | (~(a) & 0xFF000000);                  \
     }                                                                   \
     else {                                                              \
       fprintf(stderr, "Invalid RED@%d.\n",i);                           \
@@ -51,37 +51,37 @@
 
 #define FTCHK(rd,i,a) {                                                 \
     if (i == 0b0010) {                                                  \
-      rd = ((a >> 16) ^ a) & 0xFFFF;                                    \
+      rd = (((a) >> 16) ^ (a)) & 0xFFFF;                                \
     }                                                                   \
     else if (i == 0b1010) {                                             \
-      rd = (((a >> 16) ^ a) & 0xFFFF) | (~((a >> 16) ^ a) << 16) ;      \
+      rd = ((((a) >> 16) ^ (a)) & 0xFFFF) | (~(((a) >> 16) ^ (a)) << 16) ; \
     }                                                                   \
     else if (i == 0b0011) {                                             \
-      rd = ~((a >> 16) ^ a) & 0xFFFF;                                   \
+      rd = ~(((a) >> 16) ^ (a)) & 0xFFFF;                               \
     }                                                                   \
     else if (i == 0b1011) {                                             \
-      rd = (~((a >> 16) ^ a) & 0xFFFF) | (((a >> 16) ^ a) << 16);       \
+      rd = (~(((a) >> 16) ^ (a)) & 0xFFFF) | ((((a) >> 16) ^ (a)) << 16); \
     }                                                                   \
     else if (i == 0b0100) {                                             \
-      rd = (((a & 0xFF) ^ (a >> 8)) |                                   \
-            ((a & 0xFF) ^ (a >> 16)) |                                  \
-            ((a & 0xFF) ^ (a >> 24))) & 0xFF;                           \
+      rd = ((((a) & 0xFF) ^ ((a) >> 8)) |                               \
+            (((a) & 0xFF) ^ ((a) >> 16)) |                              \
+            (((a) & 0xFF) ^ ((a) >> 24))) & 0xFF;                       \
     }                                                                   \
     else if (i == 0b1100) {                                             \
-      DATATYPE tmp = (((a & 0xFF) ^ (a >> 8)) |                         \
-                      ((a & 0xFF) ^ (a >> 16)) |                        \
-                      ((a & 0xFF) ^ (a >> 24))) & 0xFF;                 \
+      DATATYPE tmp = ((((a) & 0xFF) ^ ((a) >> 8)) |                     \
+                      (((a) & 0xFF) ^ ((a) >> 16)) |                    \
+                      (((a) & 0xFF) ^ ((a) >> 24))) & 0xFF;             \
       rd = tmp | ((~tmp & 0xFF) << 8) | (tmp << 16) | ((~tmp & 0xFF) << 24); \
     }                                                                   \
     else if (i == 0b0101) {                                             \
-      rd = (((~a & 0xFF) ^ (a >> 8)) |                                  \
-            ((a & 0xFF) ^ (a >> 16)) |                                  \
-            ((~a & 0xFF) ^ (a >> 24))) & 0xFF;                          \
+      rd = (((~(a) & 0xFF) ^ ((a) >> 8)) |                              \
+            (((a) & 0xFF) ^ ((a) >> 16)) |                              \
+            ((~(a) & 0xFF) ^ ((a) >> 24))) & 0xFF;                      \
     }                                                                   \
     else if (i == 0b1101) {                                             \
-      DATATYPE tmp = ((~(a & 0xFF) ^ (a >> 8)) |                        \
-                      ((a & 0xFF) ^ (a >> 16)) |                        \
-                      (~(a & 0xFF) ^ (a >> 24))) & 0xFF;                \
+      DATATYPE tmp = ((~((a) & 0xFF) ^ ((a) >> 8)) |                    \
+                      (((a) & 0xFF) ^ ((a) >> 16)) |                    \
+                      (~((a) & 0xFF) ^ ((a) >> 24))) & 0xFF;            \
       rd = tmp | ((~tmp & 0xFF) << 8) | (tmp << 16) | ((~tmp & 0xFF) << 24); \
     }                                                                   \
     else {                                                              \
@@ -136,7 +136,7 @@ static int xorshift_rand() {
 #if FD == 2
 #define RAND() ({ DATATYPE _tmp = xorshift_rand(); (~_tmp & 0xFFFF) | (_tmp << 16); }) 
 #elif FD == 4
-#define RAND() ({ DATATYPE _tmp = xorshift_rand(); \
+#define RAND() ({ DATATYPE _tmp = xorshift_rand();                      \
       (~_tmp & 0xFF) | ((_tmp & 0xFF) << 8) | ((~_tmp & 0xFF) << 16) | (_tmp<< 24); })
 #else
 #error Invalid FD
@@ -202,7 +202,7 @@ static int xorshift_rand() {
   }
 #define TI_XOR_2(a,b,c) FD_XOR(a,b,c)
 
-#define TI_AND_4(a,b,c) {                             \
+#define TI_AND_4(a,b,c) {                           \
     DATATYPE c1, c2, c3, c4, d1, d2, d3, d4;        \
     DATATYPE r = RAND();                            \
     FD_AND(c1,b,c);    /* c1 = b & c */             \
@@ -222,15 +222,15 @@ static int xorshift_rand() {
     DATATYPE r_r1;                                  \
     TIBSROT_2(r_r1,r);                              \
     FD_XOR(d4,d3,r_r1); /* d4 = d3 ^ (r <<< 1) */   \
-    FD_XOR(a,d4,c4);    /* a  = d4 ^ c4 */      \
+    FD_XOR(a,d4,c4);    /* a  = d4 ^ c4 */          \
   }
 #define TI_NOT_4(a,b) a = (b) ^ 0x11111111
-#define TI_OR_4(a,b,c) {                          \
+#define TI_OR_4(a,b,c) {                        \
     DATATYPE notb, notc, nota;                  \
-    TI_NOT_4(notb,b);                             \
-    TI_NOT_4(notc,c);                             \
-    TI_AND_4(nota,notb,notc);                     \
-    TI_NOT_4(a,nota);                             \
+    TI_NOT_4(notb,b);                           \
+    TI_NOT_4(notc,c);                           \
+    TI_AND_4(nota,notb,notc);                   \
+    TI_NOT_4(a,nota);                           \
   }
 #define TI_XOR_4(a,b,c) FD_XOR(a,b,c)
 
