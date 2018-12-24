0084cedd80fe92a08cbc47920040777b574cef40
DadaIsCrazy <darius.mercadier@gmail.com>
2018-12-24 19:56:48 +0100

    Fix native tibs/invitbs in FAME.h

--- arch/FAME.h
+++ arch/FAME.h
@@ -132,16 +132,16 @@
 
 #else
 
-#define TIBS(rd,y,r1,r2) {                                              \
+#define TIBS(y,rd,r1,r2) {                                              \
     asm volatile("tibs %2, %3, %0\n\t"                                  \
-                 "mov %%y, %1\n\t" : "=r" (r), "=r" (y) : "r" (r1), "r" (r2)): \
+                 "mov %%y, %1\n\t" : "=r" (rd), "=r" (y) : "r" (r1), "r" (r2)); \
   }
 /* r1/r2 are actually the destination, and rd/y the source
    but it makes it clearer to name them that way (since 
    INVTIBS is the inverse of TIBS) */
-#define INVTIBS(r1,r2,rd,y) {                                           \
+#define INVTIBS(r2,r1,rd,y) {                                           \
     asm volatile("invtibs %2, %3, %0\n\t"                               \
-                 "mov %%y, %1\n\t" : "=r" (r1), "=r" (r2) : "r" (rd), "r" (y)): \
+                 "mov %%y, %1\n\t" : "=r" (r1), "=r" (r2) : "r" (rd), "r" (y)); \
   }
 
 #define RED(r,y,i,a)   {                                                \
