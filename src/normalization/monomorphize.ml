usubalang/usuba

commit 95a68e7542d14eb28b0e93bf97a3b93c1847ef9e

Fix monomorphization of function parameters
Before, if `f: (in:b4[2]) ->...` was called as `f(x,...)` where `x:b8`,
then we'd end up with `f:b8` within monomorphize, which is obviously
wrong, since `in[0]` was a `b4` but is now a `b1` etc.

This is now fixed: only the direction 'D is updated by monomorphize.

Monomorphize should also update 'm when needed; I'll have to think
about that in more depth later, but I don't think this is an issue.

src/normalization/monomorphize.ml
@@ -213,10 +213,16 @@ let specialize_expr_var (env_var:(ident, typ) Hashtbl.t) (e:expr) : expr =
 (* Given a list of var_d (p), and a list of types, update the dir/m of each
      var_d according to the corresponding type in the list of types *)
 let match_types env_dir env_m (p:p) (typs:typ list) : p =
+  (* updates the dir in tdest with the dir in tdir *)
+  let rec update_dir (tdest:typ) (tdir:typ) : typ =
+    match tdest with
+    | Nat -> tdest
+    | Uint(_,m,n) -> Uint(get_type_dir tdir,m,n)
+    | Array(t,s)  -> Array(update_dir t tdir,s) in
   List.map2 (fun vd t ->
              Hashtbl.replace env_dir (get_type_dir vd.vtyp) (get_type_dir t);
              Hashtbl.replace env_m (get_type_m vd.vtyp) (get_type_m t);
-             { vd with vtyp = t }
+             { vd with vtyp = update_dir vd.vtyp t }
             ) p typs
             
 let rec specialize_fun_call
@@ -237,6 +243,7 @@ let rec specialize_fun_call
   let ldir  = List.sort compare (values env_dir) in
   let lmtyp = List.sort compare (values env_m) in
   let f'   = gen_fun_name f ldir lmtyp in
+  
   replace_key_2nd_layer specialized_nodes f (f',ldir,lmtyp)
                         { def with
                           id    = f';

commit 540e1d95870684c9c9ce0d6a18a8b1244736cabc

Fix bug in monomorphization introduced by 95a68e7
This commit said that 'm _might need_ to be updated.
Actually, 'm _needs_ to be updated, or we end up with
non-specialized 'm, which is an error.

src/normalization/monomorphize.ml
@@ -214,15 +214,15 @@ let specialize_expr_var (env_var:(ident, typ) Hashtbl.t) (e:expr) : expr =
      var_d according to the corresponding type in the list of types *)
 let match_types env_dir env_m (p:p) (typs:typ list) : p =
   (* updates the dir in tdest with the dir in tdir *)
-  let rec update_dir (tdest:typ) (tdir:typ) : typ =
-    match tdest with
-    | Nat -> tdest
-    | Uint(_,m,n) -> Uint(get_type_dir tdir,m,n)
-    | Array(t,s)  -> Array(update_dir t tdir,s) in
+  let rec update_dir_m (t_dest:typ) (t_dir_m:typ) : typ =
+    match t_dest with
+    | Nat -> t_dest
+    | Uint(_,_,n) -> Uint(get_type_dir t_dir_m,get_type_m t_dir_m,n)
+    | Array(t,s)  -> Array(update_dir_m t t_dir_m,s) in
   List.map2 (fun vd t ->
              Hashtbl.replace env_dir (get_type_dir vd.vtyp) (get_type_dir t);
              Hashtbl.replace env_m (get_type_m vd.vtyp) (get_type_m t);
-             { vd with vtyp = update_dir vd.vtyp t }
+             { vd with vtyp = update_dir_m vd.vtyp t }
             ) p typs
             
 let rec specialize_fun_call