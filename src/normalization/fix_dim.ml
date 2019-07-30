feb5fc86501bfd9ecdfdfd02d7e6b5677121391f
DadaIsCrazy <darius.mercadier@gmail.com>
2019-07-30 21:17:41 +0200

    Fix (finally) Fix_dim.Dir_inner

--- src/normalization/fix_dim.ml
+++ src/normalization/fix_dim.ml
@@ -1,15 +1,28 @@
-(***************************************************************************** )
-                             fix_dim.ml                              
+(*********************************************************************
+   fix_dim.ml
 
-  Usuba thinks that arrays like u8x16 and u1x128 are the same thing, but they 
-  really aren't. This module fixes that.
+  Usuba thinks that arrays like u1x8[16] and u1x128 are the same
+   thing, but they really aren't. This module fixes that.
 
-( *****************************************************************************)
+  The main reason why this transformation is inlining will fail
+   without it. For instance:
+     node f(x:u1[2][3]) ...
+      let ... x[0][1] ... tel
+     node g(a:u1[6]) ...
+      let ... f(a) ... tel
+   When inlining f in g, we'd end with a[0][1], but a has type u1[6]...
+
+  Warning: The way I'm doing this for now is quite fragile and
+  can break. In particular, if you have two arrays, one of type
+  u1[a][b] and the other one of type u1[b][a], this module will
+  leave them as is, even though they should both be converted to
+  u1[a*b].
+
+ ********************************************************************)
 
 open Usuba_AST
 open Basic_utils
 open Utils
-open Printf
 
 
 module Dir_params = struct
@@ -147,115 +160,7 @@ end
 (* *)
 module Dir_inner = struct
 
-  exception Updated
-
-  let rec dim_var (vd:var) (s:int) (v:var) : var =
-    let vb = get_var_base v in
-    if vb = vd then
-      match v with
-      | Index(Index(v',i1),i2) -> Index(v',simpl_arith_ne (Op_e(Add,Op_e(Mul,i1,Const_e s),i2)))
-      | Index(v',i1) -> 
-         Range(v',simpl_arith_ne (Op_e(Mul,i1,Const_e s)),simpl_arith_ne (Op_e(Add,Op_e(Mul,i1,Const_e s),Const_e (s-1))))
-      | _ -> v
-    else v
-
-  let rec dim_expr (vd:var) (s:int) (e:expr) : expr =
-    match e with
-    | Const _        -> e
-    | ExpVar v       -> ExpVar(dim_var vd s v)
-    | Tuple l        -> Tuple(List.map (dim_expr vd s) l)
-    | Not e          -> Not(dim_expr vd s e)
-    | Shift(op,e,ae) -> Shift(op,dim_expr vd s e,ae)
-    | Log(op,x,y)    -> Log(op,dim_expr vd s x,dim_expr vd s y)
-    | Shuffle(v,l)   -> Shuffle(dim_var vd s v,l)
-    | Arith(op,x,y)  -> Arith(op,dim_expr vd s x,dim_expr vd s y)
-    | Fun(f,l)       -> Fun(f,List.map (dim_expr vd s) l)
-    | _              -> assert false
-
-  let rec dim_deq (vd:var) (s:int) (deq:deq) : deq =
-    match deq with
-    | Eqn(vs,e,sync) -> Eqn(List.map (dim_var vd s) vs, dim_expr vd s e,sync)
-    | Loop(i,ei,ef,dl,opts) -> Loop(i,ei,ef,List.map (dim_deq vd s) dl,opts)
-                                   
-  (* let dim_size (def:def) (vd:var) (s:int) : def = *)
-  (*   let vd = get_var_base vd in *)
-  (*   match def.node with *)
-  (*   | Single(vars,body) -> *)
-  (*      let vars = *)
-  (*        List.map ( *)
-  (*            fun v -> *)
-  (*            if (Var v.vid) = vd then *)
-  (*              match v.vtyp with *)
-  (*              | Array(Array(et',es2),es1) -> *)
-  (*                 { v with vtyp = Array(et',es1 * es2) } *)
-  (*              | Array(Uint(dir,m,n),es1) -> *)
-  (*                 { v with vtyp = Array(Uint(dir,m,1),es1 * n) } *)
-  (*              | _ -> assert false *)
-  (*            else v) vars in *)
-  (*      { def with node = Single(vars,List.map (dim_deq vd s) body) } *)
-  (*   | _ -> def *)
-
-  let rec collapse_inner_arrays (t:typ) : typ * int =
-    match t with
-    (* End found: Array of Array of bool *)
-    | Array( Array( Uint(d,m,1), es2 ), es1 ) ->
-       Array( Uint(d,m,1), es2 * es1 ), es1
-    (* End found: Array of bm *)
-    | Array( Uint(d,m,n), es1 ) ->
-       Array( Uint(d,m,1), es1 * n ), es1
-    (* Not the end, going deeper *)
-    | Array(t',es1) ->
-       let (t',size) = collapse_inner_arrays t' in
-       Array(t',es1), size
-    (* Can't be a Uint or a Nat *)
-    | _ -> assert false
-
-  let rec unnest_var (def:def) (var:var) : def =
-    Printf.printf "Unnest_var %s\n" (Usuba_print.var_to_str var);
-    let var_base = get_var_base var in
-    match def.node with
-    | Single(vars,body) ->
-       (* size: the size of the arrays being collapsed. For instance,
-            b1[3][5] -> size = 5 *)
-       let size = ref (-1) in
-       (* new_type: the type of 'var' after this transformation *)
-       let new_type = ref Nat in
-
-       (* Updating 'var' type in 'vars' *)
-       let vars = List.map (fun v ->
-                            if (Var v.vid) = var_base then
-                              (Printf.printf "Found it: %s\n" (Usuba_print.vd_to_str v);
-                               let (t',s) = collapse_inner_arrays v.vtyp in
-                               new_type := t';
-                               size     := s;
-                               { v with vtyp = t' })
-                               (* match v.vtyp with *)
-                               (* | Array(Array(t, es2), es1) -> *)
-                               (*    new_type := Array(t, es1 * es2); *)
-                               (*    size     := es1; *)
-                               (*    { v with vtyp = !new_type } *)
-                               (* | Array(Uint(dir,m,n), es1) -> *)
-                               (*    new_type := Array(Uint(dir,m,1), es1 * n); *)
-                               (*    size     := es1; *)
-                               (*    { v with vtyp = !new_type } *)
-                               (* (\* 'v' isn't an array -> Not possible *\) *)
-                               (* | _ -> assert false) *)
-                            else v) vars in
-       { def with node = Single(vars, List.map (dim_deq var_base !size) body) }
-    | _ -> assert false
-    (*    (\* Updating body by expanding 'var' within *\) *)
-    (*    let def = { def with node = Single(vars, List.map (dim_deq var_base !size) body) } in *)
-    (*    (\* Checking if need to unnest further *\) *)
-    (*    (match !new_type with *)
-    (*     | Uint(_,_,1) -> def *)
-    (*     (\* If not a u1, then need to unnest further *\) *)
-    (*     | Array _ | Uint _ -> unnest_var def var *)
-    (*     (\* Can't be a Nat -> we wouldn't be expanding it *\) *)
-    (*     | Nat -> assert false) *)
-    (* (\* Not a 'Single'. Shouldn't happend. *\) *)
-    (* | _ -> assert false *)
-       
-    
+  exception Updated    
 
   (* Returns the "nested" level of a type: it corresponds to how many nested arrays there
      are in the type. For instance:
@@ -270,14 +175,127 @@ module Dir_inner = struct
     | Uint(_,_,n) -> 1
     | Array(t',_) -> 1 + (get_nested_level t')
 
+  let rec get_index_level (v:var) : int =
+    match v with
+    | Var _ -> 0
+    | Index(v',_) -> 1 + (get_index_level v')
+    | _ -> (* Usuba0, can't have Slice/Range *)
+       assert false
+                   
+  (* Collapses the last two Index of |v|.
+     Need to be careful that they actually need to be collapsed. For
+     instance, consider x:u1[2][3][4]: 
+       (1) x[1] should not be changed,
+       (2) x[1][2] should be come x[1][8..11],
+       (3) x[1][2][3] should become x[1][11].  
+   *)
+  let rec dim_var (v_tgt:var) (dim:int) (size:int) (v:var) : var =
+    if get_var_base v = v_tgt then
+      let index_lvl = get_index_level v in
+      (* Depending on |dim|-|index_lvl|, a different transformation
+         must be done (see the function's comment above) *)
+      assert (dim >= index_lvl);
+      match dim - index_lvl with
+      | 0 -> (* case (3) in the comment above *)
+         (match v with
+          | Index(Index(v',i1),i2) ->
+             Index(v',simpl_arith_ne (Op_e(Add,Op_e(Mul,i1,Const_e size),i2)))
+          | _ -> assert false)
+      | 1 -> (* case (2) in the comment above *)
+         (match v with
+          | Index(v',i1) ->
+             Range(v',simpl_arith_ne (Op_e(Mul,i1,Const_e size)),
+                   simpl_arith_ne (Op_e(Add,Op_e(Mul,i1,Const_e size),
+                                        Const_e (size-1))))
+          | _ -> assert false)
+      | _ -> v (* case (1) in the comment above *)
+    else (* v != v_tgt *) v
+
+  let rec dim_expr (v_tgt:var) (dim:int) (size:int) (e:expr) : expr =
+    match e with
+    | Const _        -> e
+    | ExpVar v       -> ExpVar(dim_var v_tgt dim size v)
+    | Tuple l        -> Tuple(List.map (dim_expr v_tgt dim size) l)
+    | Not e          -> Not(dim_expr v_tgt dim size e)
+    | Shift(op,e,ae) -> Shift(op,dim_expr v_tgt dim size e,ae)
+    | Log(op,x,y)    -> Log(op,dim_expr v_tgt dim size x,dim_expr v_tgt dim size y)
+    | Shuffle(v,l)   -> Shuffle(dim_var v_tgt dim size v,l)
+    | Arith(op,x,y)  -> Arith(op,dim_expr v_tgt dim size x,dim_expr v_tgt dim size y)
+    | Fun(f,l)       -> Fun(f,List.map (dim_expr v_tgt dim size) l)
+    | _              -> assert false
+
+  (* Updates |v_tgt| (for 'var_target') by merging its two innermost arrays *)
+  let rec dim_deq (v_tgt:var) (dim:int) (size:int) (deq:deq) : deq =
+    match deq with
+    | Eqn(vs,e,sync) ->
+       Eqn(List.map (dim_var v_tgt dim size) vs,
+           dim_expr v_tgt dim size e,sync)
+    | Loop(i,ei,ef,dl,opts) ->
+       Loop(i,ei,ef,List.map (dim_deq v_tgt dim size) dl,opts)
+
+  (* Merges the two innermost dimensions of |t|. For instance:
+      u1[2][3]    -> u1[6]
+      u1[2][3][4] -> u1[2][12]
+      b8[2]       -> b1[16] (b16 would also work) 
+      b8[2][3]    -> b1[2][24] (b24|2] would also work)
+   *)
+  let rec collapse_inner_arrays (t:typ) : typ * int =
+    match t with
+    (* End found: Array of Array of bool *)
+    | Array( Array( Uint(d,m,1), es2 ), es1 ) ->
+       Array( Uint(d,m,1), es2 * es1 ), es1
+    (* End found: Array of bm *)
+    | Array( Uint(d,m,n), es1 ) ->
+       Array( Uint(d,m,1), es1 * n ), n
+    (* Not the end, going deeper *)
+    | Array(t',es1) ->
+       let (t',size) = collapse_inner_arrays t' in
+       Array(t',es1), size
+    (* Can't be a Uint or a Nat *)
+    | _ -> assert false
+
+  (* |v| is an array with dimension > 1.
+     unnest_var will collapse its last 2 dimensions.
+     For instance, u1[2][3][4] would become u1[2][12].
+   *)
+  let rec unnest_var (def:def) (var:var) : def =
+    let var_base = get_var_base var in
+    match def.node with
+    | Single(vars,body) ->
+       (* |size|: the size of the arrays being collapsed. For instance,
+            b1[3][5] -> size = 5 
+          (used to compute indices in the new array: 
+            x[a][b] becomes x[a*size+b]) *)
+       let size = ref (-1) in
+       (* |new_type|: the type of |var| after  this transformation *)
+       let new_type = ref Nat in
+       (* |old_type|: the type of |var| before this transformation *)
+       let old_type = ref Nat in
+       
+       (* Updating |var| type in |vars| *)
+       let vars =
+         List.map
+           (fun v ->
+             if Var v.vid = var_base then
+               (let (t',s) = collapse_inner_arrays v.vtyp in
+                old_type := v.vtyp;
+                new_type := t';
+                size     := s;
+                { v with vtyp = t' })
+             else v) vars in
+       (* Updating body with new var *)
+       { def with
+         node = Single(vars, List.map (dim_deq var_base
+                                         (get_nested_level !old_type) !size)
+                               body) }
+    | _ -> assert false       
+
              
-  (* *)
   let rec fix_deqs env_fun env_var (def:def) (deqs:deq list) : deq list =
     List.map
       (fun deq ->
        match deq with
        | Eqn(ret_vars,Fun(f,args),sync) when f.name <> "rand" ->
-          (* Printf.printf "Considering: %s\n" (Usuba_print.deq_to_str deq); *)
           (* A funcall -> need to:
               - iterate over arguments to make sure they have the correct types
               - iterate over return values to make sure they have the correct types
@@ -291,42 +309,22 @@ module Dir_inner = struct
             (fun i arg ->
              match arg with
              (* A parameter can be either a Const or an ExpVar
-                                (since we are in Usuba0)                        *)
+                                (since we are in Usuba0)       *)
              | Const _ -> ()
              | ExpVar v ->
                 (* type of the i-th argument *)
                 let arg_type  = get_var_type env_var v  in
                 (* type of the i-th expected parameter *)
                 let exp_type  = (List.nth p_in i).vtyp  in
-
-                (* Printf.printf "fixing %s: %s-%s\n" def.id.name *)
-                (*               (Usuba_print.typ_to_str arg_type) *)
-                (*               (Usuba_print.typ_to_str exp_type); *)
-                
+                                
                 if (get_nested_level arg_type) > (get_nested_level exp_type) then (
                   (* Different sizes, need convert arg to a non-nested array *)
                   Hashtbl.replace env_fun def.id (unnest_var def v);
                   raise Updated
-                                  
-                  (* match arg_type with *)
-                  (* | Array(Array(_,es1),_) -> *)
-                  (*    (match exp_type with *)
-                  (*     | Array _ *)
-                  (*     | Uint _ -> *)
-                  (*        let deq' = dim_size def v es1 in *)
-                  (*        Hashtbl.replace env_fun def.id deq'; *)
-                  (*        raise Updated *)
-                  (*     | _ -> ()) *)
-                  (* | Array(Uint(_,_,n),_) when n > 1 -> *)
-                  (*    (match vtyp with *)
-                  (*     | Array _ *)
-                  (*     | Uint _-> *)
-                  (*        let deq' = dim_size def v n in *)
-                  (*        Hashtbl.replace env_fun def.id deq'; *)
-                  (*        raise Updated *)
-                  (*     | _ -> ()) *)
-                  (* (\* arg type isn't an array -> nothing to do *\) *)
-                  (* | _ -> () *)
+
+                (* < isn't checked here: if dim is < than function
+                   expects, then the function must be adapted
+                   (we can always reduce dimension, but not expand) *)
                 ) else ()
              (* Not a Const/ExpVar -> not possible *)
              | _ -> assert false
@@ -335,48 +333,32 @@ module Dir_inner = struct
           (* Retrieveing f's returns param, and checking return values' types *)
           let p_out = (Hashtbl.find env_fun f).p_out in
           List.iteri (fun i v ->
-                      let ret_type = get_var_type env_var v  in
-                      let exp_type = (List.nth p_out i).vtyp in
-                      if (get_nested_level ret_type) > (get_nested_level exp_type) then (
-                        (* Different sizes, need convert arg to a non-nested array *)
-                        Hashtbl.replace env_fun def.id (unnest_var def v);
-                        raise Updated
-                      ) else ()
-                      (* match arg_type with *)
-                      (*        | Array(Array(_,es1),_) -> *)
-                      (*           (match vtyp with *)
-                      (*            | Array _ *)
-                      (*            | Uint _ -> *)
-                      (*               let deq' = dim_size def v es1 in *)
-                      (*               Hashtbl.replace env_fun def.id deq'; *)
-                      (*               raise Updated *)
-                      (*            | _ -> ()) *)
-                      (*        | Array(Uint(_,_,n),_) when n > 1 -> *)
-                      (*           (match vtyp with *)
-                      (*            | Array _ *)
-                      (*            | Uint _ -> *)
-                      (*               let deq' = dim_size def v n in *)
-                      (*               Hashtbl.replace env_fun def.id deq'; *)
-                      (*               raise Updated *)
-                      (*            | _ -> ())                               *)
-                      (*        | _ -> () *)
-                            ) ret_vars;
-                 deq
-              (* A simple equation can't contain funcall -> ignore *)
-              | Eqn(vs,e,sync)        -> deq
-              (* Reccursive call on loops *)
-              | Loop(i,ei,ef,dl,opts) ->
-                 Hashtbl.add env_var i Nat;
-                 let res = Loop(i,ei,ef,fix_deqs env_fun env_var def dl,opts) in
-                 Hashtbl.remove env_var i;
-                 res) deqs
+              (* type of the i-th lhs variable *)
+              let ret_type = get_var_type env_var v  in
+              (* type of the i-th returned variable *)
+              let exp_type = (List.nth p_out i).vtyp in
+              if (get_nested_level ret_type) > (get_nested_level exp_type) then (
+                (* Different sizes, need convert arg to a non-nested array *)
+                Hashtbl.replace env_fun def.id (unnest_var def v);
+                raise Updated
+              ) else ()
+            ) ret_vars;
+          deq
+       (* A simple equation can't contain funcall -> ignore *)
+       | Eqn(vs,e,sync) -> deq
+       (* Reccursive call on loops *)
+       | Loop(i,ei,ef,dl,opts) ->
+          Hashtbl.add env_var i Nat;
+          let res = Loop(i,ei,ef,fix_deqs env_fun env_var def dl,opts) in
+          Hashtbl.remove env_var i;
+          res) deqs
 
   let rec fix_def env_fun (def:def) : unit =
     try
       match def.node with
       | Single(vars,body) ->
-         let env_var     =  build_env_var def.p_in def.p_out vars  in
-         let body        =  fix_deqs env_fun env_var def body      in
+         let env_var =  build_env_var def.p_in def.p_out vars in
+         let body    =  fix_deqs env_fun env_var def body     in
          Hashtbl.replace env_fun def.id
                          { def with node = Single(vars, body) }
       | _ -> ()
@@ -388,7 +370,7 @@ module Dir_inner = struct
                   false = keep loops
                   true  = keep arrays in parameters
                   So basically, this combination of (0,false,true) means
-                  "just expand ranges", and anything only if _needed_. *)
+                  "just expand ranges", and anything else only if needed. *)
     
                             
              
