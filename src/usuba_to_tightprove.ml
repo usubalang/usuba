e5ebce3b6d564f4760fe7ac352ac79e321a67039
DadaIsCrazy <darius.mercadier@gmail.com>
2019-07-30 11:24:29 +0200

    Support mslicing for tp

--- src/usuba_to_tightprove.ml
+++ src/usuba_to_tightprove.ml
@@ -1,7 +1,11 @@
 open Usuba_AST
 open Basic_utils
+open Utils
 open Printf
 
+(* true if bitslicing; false if vslicing *)
+let bitslice = ref true
+   
 let ident_to_str id = 
   Str.global_replace (Str.regexp "'") "_" id.name
 
@@ -23,6 +27,12 @@ let log_op_to_str = function
   | Or  -> "or"
   | Xor -> "xor"
   | Andn -> assert false
+
+let shift_op_to_str = function
+  | Lshift -> "<<"
+  | Rshift -> ">>"
+  | Lrotate -> "<<<"
+  | Rrotate -> ">>>"
                             
 let rec var_to_str = function
   | Var v -> ident_to_str v
@@ -31,12 +41,18 @@ let rec var_to_str = function
   | Slice(v,l) -> sprintf "%s[%s]" (var_to_str v) (join "," (List.map arith_to_str l))
                               
 let rec expr_to_str = function
+  | Const c -> if !bitslice then sprintf "setcstall(%d)" c
+               else sprintf "setcst(0x%x)" c
   | ExpVar v   -> var_to_str v
   | Log(o,x,y) -> sprintf "%s %s %s" 
-                          (log_op_to_str o) (expr_to_str x) (expr_to_str y)
+                    (log_op_to_str o) (expr_to_str x) (expr_to_str y)
+  | Shift(op,e,ae) -> sprintf "%s %s %s"
+                    (expr_to_str e) (shift_op_to_str op) (arith_to_str ae)
   | Not e -> sprintf "not %s" (expr_to_str e)
   | Fun(f,l) -> sprintf "%s(%s)" f.name (join "," (List.map expr_to_str l))
-  | _ -> assert false
+  | e -> Printf.fprintf stderr "expr_to_str: invalid expr `%s`\n"
+           (Usuba_print.expr_to_str e);
+         assert false
 
 let pat_to_str pat =
   match pat with
@@ -49,10 +65,38 @@ let rec deq_to_str = function
                             (pat_to_str pat)
                             (expr_to_str e)
   | _ -> assert false
-                                                                
+
+let rec vd_typ_to_str (typ:typ) (acc:string) : string =
+  match typ with
+  | Uint(_,_,1)   -> sprintf "%s" acc
+  | Uint(_,_,n)   -> sprintf "%s[%d]" acc n
+  | Array(typ',n) -> vd_typ_to_str typ' (sprintf "[%d]%s" n acc)
+  | _ -> assert false
+                   
+let vd_to_str (vd:var_d) : string =
+  sprintf "%s%s" (ident_to_str vd.vid)
+    (vd_typ_to_str vd.vtyp "")
+
+let m_as_int = function
+  | Mint m -> m
+  | _ -> assert false
+       
 let single_node_to_str (id:ident) (p_in:p) (p_out:p) (vars:p) (deq:deq list) =
-  (join "" (List.map (fun vd -> sprintf "in %s\n" (ident_to_str vd.vid)) p_in)) ^
-    (join "" (List.map deq_to_str deq))
+  let m = m_as_int (get_type_m (List.hd p_in).vtyp) in
+  bitslice := m = 1;
+  sprintf
+"rs=%d
+
+in %s
+
+%s
+"
+(* m *)
+m
+(* inputs *)
+(join " " (List.map vd_to_str p_in))
+(* body *)
+(join "" (List.map deq_to_str deq))
       
 let def_to_str (def:def) =
   match def.node with
