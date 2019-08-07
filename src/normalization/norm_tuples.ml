34b6d999af1492a56da0866a94360c127f38b35c
DadaIsCrazy <darius.mercadier@gmail.com>
2019-08-07 23:24:04 +0200

    Clean Norm_tuples
    Three of the modules could actually be merge into a much simpler one.
    
    Split_tuples will need to be reworked a bit once Unfold_unnest and
    expand_var have been reworked (gonna happen in the next few commits).

--- src/normalization/norm_tuples.ml
+++ src/normalization/norm_tuples.ml
@@ -2,80 +2,85 @@ open Usuba_AST
 open Basic_utils
 open Utils
 
-       
-(* Removes tuples of 1 element *)
-module Remove_tuples_funcall = struct
 
-  let rec simpl_tuple (t:expr) : expr =
-    match t with
-    | Tuple l -> Tuple(List.map simpl_tuple l)
-    | Not e -> Not (simpl_tuple e)
-    | Shift(op,e,n) -> Shift(op,simpl_tuple e,n)
-    | Log(op,x,y) -> Log(op,simpl_tuple x,simpl_tuple y)
-    | Arith(op,x,y) -> Arith(op,simpl_tuple x,simpl_tuple y)
-    | Fun(f,[Tuple l]) -> Fun(f,List.map simpl_tuple l)
-    | Fun(f,l) -> Fun(f,List.map simpl_tuple l)
-    | Fun_v(f,n,l) -> Fun_v(f,n,List.map simpl_tuple l)
-    | _ -> t
-
-  let rec simpl_deqs (deq:deq list) : deq list =
-    List.map (function
-               | Eqn(p,e,sync) -> Eqn(p,simpl_tuple e,sync)
-               | Loop(i,ei,ef,dl,opts) -> Loop(i,ei,ef,simpl_deqs dl,opts)) deq
-             
-  let simpl_tuples_def (def: def) : def =
-    match def.node with
-    | Single(p_var,body) ->
-       { def with
-         node  = Single(p_var, simpl_deqs body) }
-    | _ -> def
-                     
-  let remove_tuples_funcall (p: prog) : prog =
-    { nodes = List.map simpl_tuples_def p.nodes }
-end
-                           
-(* Removes tuples of 1 element *)
+(* Simplify (ie, remove) every tuples that can be simplified:
+     - Remove Tuples from function call arguments:
+         f(a,(b,c)) == f(a,b,c)
+     - Flatten Tuples of Tuples:
+         (a,(b,c)) == (a,b,c)
+     - Remove Tuples of one element
+         (e) == e
+*)
 module Simplify_tuples = struct
 
-  let rec simpl_tuple (t:expr) : expr =
-    match t with
-    | Tuple l -> if List.length l = 1 then List.nth l 0
-                 else (match List.map simpl_tuple l with
-                       | x::[] -> x
-                       | l -> Tuple(l))
-    | Not e -> Not (simpl_tuple e)
-    | Shift(op,e,n) -> Shift(op,simpl_tuple e,n)
-    | Log(op,x,y) -> Log(op,simpl_tuple x,simpl_tuple y)
-    | Arith(op,x,y) -> Arith(op,simpl_tuple x,simpl_tuple y)
-    | Fun(f,l) -> Fun(f,List.map simpl_tuple l)
-    | Fun_v(f,n,l) -> Fun_v(f,n,List.map simpl_tuple l)
-    | _ -> t
+  (* Builds an expression from an list of expression: if the list
+     contains a single element, then return it, otherwise, wrap the
+     list in a Tuple. *)
+  let expr_from_list (el:expr list) : expr =
+    match el with
+    | e :: [] -> e
+    | el      -> Tuple el
+
+  (* Returns a list of expression from an expression. The caller
+     should then take care or rebuilding an expression from this list
+     using expr_from_list.
+
+     Note that in the spirit of "sticking to one job", this function
+     doesn't perform any kind of expansion/unfolding: for instance, if
+     a Not is applied to a Tuple, then let it be. Some other module
+     will take care of distributing the Not on every elements of the
+     Tuple.  *)
+  let rec simpl_tuple (e:expr) : expr list =
+    match e with
+    | Tuple l -> flat_map simpl_tuple l
+    | Not e   -> [ Not (expr_from_list (simpl_tuple e)) ]
+    | Shift(op,e,n) -> [ Shift(op,expr_from_list (simpl_tuple e),n) ]
+    | Log(op,x,y)   -> [ Log(op,expr_from_list (simpl_tuple x),
+                             expr_from_list (simpl_tuple y)) ]
+    | Arith(op,x,y) -> [ Arith(op, expr_from_list (simpl_tuple x),
+                               expr_from_list (simpl_tuple y)) ]
+    | Fun(f,l)     ->
+       (* If |l| is a Tuple, then the reccursive call goes into Tuple,
+          effectively removing the Tuple. *)
+       [ Fun(f,flat_map simpl_tuple l) ]
+    | Fun_v(f,n,l) -> [ Fun_v(f,n,flat_map simpl_tuple l) ]
+    | _ -> [ e ]
 
   let rec simpl_deqs (deq:deq list) : deq list =
     List.map (function
-               | Eqn(p,e,sync) -> Eqn(p,simpl_tuple e,sync)
-               | Loop(i,ei,ef,dl,opts) -> Loop(i,ei,ef,simpl_deqs dl,opts)) deq
-             
+        | Eqn(p,e,sync) -> Eqn(p,expr_from_list (simpl_tuple e),sync)
+        | Loop(i,ei,ef,dl,opts) -> Loop(i,ei,ef,simpl_deqs dl,opts)) deq
+
   let simpl_tuples_def (def: def) : def =
     match def.node with
     | Single(p_var,body) ->
-       { id    = def.id;
-         p_in  = def.p_in;
-         p_out = def.p_out;
-         opt   = def.opt;
-         node  = Single(p_var, simpl_deqs body) }
+       { def with node  = Single(p_var, simpl_deqs body) }
     | _ -> def
-                     
-  let simplify_tuples (p: prog) : prog =
-    { nodes = List.map simpl_tuples_def p.nodes }
+
+  let simpl_tuples (prog: prog) : prog =
+    { nodes = List.map simpl_tuples_def prog.nodes }
+
 end
 
-(* Split tuples into atomic operations, if possible *)
+(* Split of tuples into assignements of non-tuples:
+       (x1,x2,x3) = (a,b,c)
+     becomes
+       x1 = a;
+       x2 = b;
+       x3 = c;
+   This also works for assigments like
+       x = (a,b,c)
+     which becomes
+       x[0] = a;
+       x[1] = b;
+       x[2] = c;
+ *)
 module Split_tuples = struct
   let real_split_tuple env (p: var list) (l: expr list) (sync:bool) : deq list =
+    (* TODO: don't expand here. probably. *)
     List.map2 (fun l r -> Eqn([l],r,sync)) (flat_map (expand_var env) p)
               (flat_map (Unfold_unnest.expand_expr env) l)
-               
+
   let rec split_tuples_deq env (body: deq list) : deq list =
     flat_map
       (fun x ->
@@ -95,68 +100,27 @@ module Split_tuples = struct
        let env = build_env_var def.p_in def.p_out p_var in
        { def with node  = Single(p_var, split_tuples_deq env body) }
     | _ -> def
-                 
-  let split_tuples (p: prog) : prog =
-    { nodes = List.map split_tuples_def p.nodes }
+
+  let split_tuples (prog: prog) : prog =
+    { nodes = List.map split_tuples_def prog.nodes }
 end
 
+let rec norm_tuples_deq (def:def) : def =
+  let def' =
+    Simplify_tuples.simpl_tuples_def
+      (Split_tuples.split_tuples_def
+         (Simplify_tuples.simpl_tuples_def def)) in
 
-(* Flatten tuples == removes nested tuples
-    ((a,b),c) ==> (a,b,c) *)
-module Flatten_tuples = struct
+  (* Fixpoint to make sure every tuples are complitely simplified. *)
+  if def <> def' then norm_tuples_deq def'
+  else def
 
-  let rec flatten_tuples_expr (e:expr) : expr =
-    let rec rec_call_list (l:expr list) : expr list =
-      flat_map (fun x -> match flatten_tuples_expr x with
-                         | Tuple l' -> l'
-                         | x -> [ x ]) l in
-    match e with
-    | Const _ | ExpVar _ | Shuffle _ -> e
-    | Tuple l -> Tuple (rec_call_list l)
-    | Not e' -> Not (flatten_tuples_expr e')
-    | Shift(op,e',ae) -> Shift(op,flatten_tuples_expr e',ae)
-    | Log(op,x,y) -> Log(op,flatten_tuples_expr x,flatten_tuples_expr y)
-    | Arith(op,x,y) -> Arith(op,flatten_tuples_expr x,flatten_tuples_expr y)
-    | Fun(f,l) -> Fun(f,rec_call_list l)
-    | Fun_v(f,ae,l) -> Fun_v(f,ae,rec_call_list l)
-    | _ -> assert false
-  
-  let rec flatten_tuples_deq (body:deq list) : deq list =
-    List.map (function
-               | Eqn(p,e,sync) -> Eqn(p,flatten_tuples_expr e,sync)
-               | Loop(i,ei,ef,dl,opts) -> Loop(i,ei,ef,flatten_tuples_deq dl,opts)) body
-  
-  let flatten_tuples_def (def:def) : def =
-    match def.node with
-    | Single(p_var,body) ->
-       { def with node = Single(p_var,flatten_tuples_deq body) }
-    | _ -> def
-  
-  let flatten_tuples (p:prog) : prog =
-    { nodes = List.map flatten_tuples_def p.nodes }
-end
+let rec norm_tuples (prog:prog) (conf:config) : prog =
+  let prog' =
+    Simplify_tuples.simpl_tuples
+      (Split_tuples.split_tuples
+         (Simplify_tuples.simpl_tuples prog)) in
 
-let norm_tuples_deq (def:def) : def =
-  (* Dunno if I should loop for a fixpoint or not... 
-     For now, this should be sufficient. *)
-  def |>
-    Simplify_tuples.simpl_tuples_def |>
-    Split_tuples.split_tuples_def |>
-    Flatten_tuples.flatten_tuples_def |>
-    Remove_tuples_funcall.simpl_tuples_def |>
-    Simplify_tuples.simpl_tuples_def |>
-    Split_tuples.split_tuples_def |>
-    Remove_tuples_funcall.simpl_tuples_def
-
-let norm_tuples (prog:prog) (conf:config) : prog =
-  (* Dunno if I should loop for a fixpoint or not... 
-     For now, this should be sufficient. *)
-  prog |>
-    Simplify_tuples.simplify_tuples |>
-    Split_tuples.split_tuples |>
-    Flatten_tuples.flatten_tuples |>
-    Remove_tuples_funcall.remove_tuples_funcall |>
-    Simplify_tuples.simplify_tuples |>
-    Split_tuples.split_tuples |>
-    Remove_tuples_funcall.remove_tuples_funcall
-  
+  (* Fixpoint to make sure every tuples are complitely simplified. *)
+  if prog <> prog' then norm_tuples prog' conf
+  else prog'
