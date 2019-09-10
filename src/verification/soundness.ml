85479a34cb45039f98d6f483945b13b8c33117fd
DadaIsCrazy <darius.mercadier@gmail.com>
2019-09-10 20:37:36 +0200

    Update table verification with new compiler infra

--- src/verification/soundness.ml
+++ src/verification/soundness.ml
@@ -6,11 +6,11 @@ open Printf
 exception Unsound of string
 
 let print_env env = Hashtbl.fold (fun k _ _ -> printf "%s\n" k) env ()
-                       
+
 
 let compare_tables (orig:def) (norm:def)  =
   let nb_inputs = p_size norm.p_in in
-  
+
   let out_size = p_size orig.p_out in
   for i = 0 to (pow 2 out_size) - 1 do
     let input = int_to_boollist i nb_inputs in
@@ -19,15 +19,18 @@ let compare_tables (orig:def) (norm:def)  =
     if out_orig <> out_norm then
       raise (Unsound (Printf.sprintf "%s: %d => expect:%d -- got:%d" orig.id.name i
                                      (boollist_to_int out_orig)
-                                     (boollist_to_int out_norm)))    
+                                     (boollist_to_int out_norm)))
   done
   (* Printf.fprintf stderr "Table %s sound.\n" orig.id.name *)
-    
+
 
 let tables_sound (orig:prog) (normalized:prog) : unit =
-  let tables = Hashtbl.create 10 in
-  List.iter (fun x -> match x.node with
-                      | Table _ -> env_add tables x.id x
-                      | _ -> ()) (Expand_multiples.expand_multiples orig default_conf).nodes;
-  List.iter (fun x -> if env_contains tables x.id then
-                        compare_tables (Hashtbl.find tables x.id.name) x) normalized.nodes;
+
+  List.iter (fun x ->
+             match x.node with
+             | Table _ ->
+                (try let normed = List.find (fun def -> contains def.id.name x.id.name)
+                                            normalized.nodes in
+                     compare_tables x normed
+                 with Not_found -> ())
+             | _ -> ()) (Expand_multiples.expand_multiples orig default_conf).nodes
