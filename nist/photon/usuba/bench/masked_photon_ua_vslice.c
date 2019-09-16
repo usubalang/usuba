ee9986b28606da4d63e3a6c59f98c0bdf63541ef
Pierre-Évariste Dagand <pierre-evariste.dagand@lip6.fr>
2019-09-16 14:23:41 +0200

    Add dummy vsliced implementations

---
+++ nist/photon/usuba/bench/masked_photon_ua_vslice.c
@@ -0,0 +1,10 @@
+/* DUMMY PLACEHOLDER
+
+   There is no naturally vsliced version of photon and my Makefile is
+   not clever enough to know that. */
+
+#include <stdint.h>
+
+uint32_t bench_speed() {
+  return 1;
+}
