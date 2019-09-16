0b491493a1093ce58593a3a5f11d7f82374afb9c
Pierre-Évariste Dagand <pierre-evariste.dagand@lip6.fr>
2019-09-16 14:23:41 +0200

    Add dummy vsliced implementations

---
+++ nist/spongent/usuba/bench/masked_spongent_ua_vslice.c
@@ -0,0 +1,10 @@
+/* DUMMY PLACEHOLDER
+
+   There is no vsliced version of spongent and my Makefile is not
+   clever enough to know that. */
+
+#include <stdint.h>
+
+uint32_t bench_speed() {
+  return 1;
+}
