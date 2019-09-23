bd084d7030ba6e6cd5bcd9c31d0d6d35ac6aa66d
DadaIsCrazy <darius.mercadier@gmail.com>
2019-09-23 20:06:08 +0200

    Update bench_nist.pl to be more configurable

--- bench_nist.pl
+++ bench_nist.pl
@@ -8,10 +8,12 @@ use Data::Printer;
 
 use Getopt::Long;
 
-my ($pattern, $by_cipher, $by_slicing) = ("");
+my ($pattern, $nb_run, $warmup, $by_cipher, $by_slicing) = ("", 100000, 10000);
 GetOptions("by-cipher"  => \$by_cipher,
            "by-slicing" => \$by_slicing,
-           "pattern=s"  => \$pattern);
+           "pattern=s"  => \$pattern,
+           "nb-run=i"   => \$nb_run,
+           "warmup=i"   => \$warmup);
 
 # Setting by_slicing by default
 if (!$by_cipher && !$by_slicing) { $by_slicing = 1 }
@@ -23,7 +25,7 @@ for my $code (glob("nist/*/usuba/bench/*{{bit,v}slice,ref}.c")) {
     next if $pattern && $code !~ /$pattern/;
 
     print "\r\033[2KCompiling $code.....";
-    system "clang -O3 -march=native -I arch -o _run experimentations/bench_generic/bench.c $code";;
+    system "clang -O3 -march=native -I arch -o _run experimentations/bench_generic/bench.c $code -D NB_RUN=$nb_run -D WARMING=$warmup";;
     print " done.";
 
     print "\r\033[2KRunning $code.....";
