3baf2afd63a98e7fddf0ae05688ec5c973d54d29
DadaIsCrazy <darius.mercadier@gmail.com>
2019-09-24 21:36:04 +0200

    Rework slightly loop fusion

--- src/optimization/fuse_loops.ml
+++ src/optimization/fuse_loops.ml
@@ -52,33 +52,60 @@ let rec add_to_skipped (skipped:(ident,bool) Hashtbl.t)
 let rec partition_deqs ?(skipped:(ident,bool) Hashtbl.t=Hashtbl.create 10)
                        (outer:loop) (nexts:deq list) :
           deq list  =
-  flat_map (fun deq ->
-            match deq with
-            | Eqn(lhs,_,_) ->
-               add_to_skipped skipped deq;
-               [ deq ]
-            | Loop(i,ei,ef,dl,_) ->
-               if (* For now, ignoring any loop that doesn't use
+  if false then
+    (* This first version is less aggressive when fusing loops: it
+       only fuses that are right next to each others. *)
+  match nexts with
+  | [] -> []
+  | (Eqn _) :: _ -> nexts
+  | (Loop(i,ei,ef,dl,_)) :: tl ->
+     if (* For now, ignoring any loop that doesn't use
                           the same iterator. An improvement would be
                           to check the size of the loop instead, and
                           if two loops have the same size, fuse
                           them. TODO! *)
-                 ((outer.id = i) && (outer.ei = ei) && (outer.ef = ef)) &&
-                   (* This loop has the same iterator as the
+       ((outer.id = i) && (outer.ei = ei) && (outer.ef = ef)) &&
+         (* This loop has the same iterator as the
                            current loop. We need to make sure that:
                              - it doesn't rely on variables that are not
                                ready
                              - there is no conficts with current loop.
                            If both conditions are satisfied, merge it. *)
-                   (* (is_ready_loop env_var env_it env_ready loop) && *)
-                   (is_mergeable skipped dl) then
-                 ((* Quadratic insertion; OK for now. *)
-                   outer.dl <- outer.dl @ dl;
-                   [])
-               else
-                 (add_to_skipped skipped deq;
-                  [ deq ])
-           ) nexts
+         (* (is_ready_loop env_var env_it env_ready loop) && *)
+         (is_mergeable skipped dl) then
+       ((* Quadratic insertion; OK for now. *)
+         outer.dl <- outer.dl @ dl;
+         partition_deqs ~skipped:skipped outer tl)
+     else
+       nexts
+  else
+    flat_map (fun deq ->
+              match deq with
+              | Eqn(lhs,_,_) ->
+                 add_to_skipped skipped deq;
+                 [ deq ]
+              | Loop(i,ei,ef,dl,_) ->
+                 if (* For now, ignoring any loop that doesn't use
+                          the same iterator. An improvement would be
+                          to check the size of the loop instead, and
+                          if two loops have the same size, fuse
+                          them. TODO! *)
+                   ((outer.id = i) && (outer.ei = ei) && (outer.ef = ef)) &&
+                     (* This loop has the same iterator as the
+                           current loop. We need to make sure that:
+                             - it doesn't rely on variables that are not
+                               ready
+                             - there is no conficts with current loop.
+                           If both conditions are satisfied, merge it. *)
+                     (* (is_ready_loop env_var env_it env_ready loop) && *)
+                     (is_mergeable skipped dl) then
+                   ((* Quadratic insertion; OK for now. *)
+                     outer.dl <- outer.dl @ dl;
+                     [])
+                 else
+                   (add_to_skipped skipped deq;
+                    [ deq ])
+             ) nexts
 
 
 let rec fuse_loops_deqs (deqs:deq list) : deq list =
