3dd1f105fe66a7aed630a679fbfff65cfd9d5944
DadaIsCrazy <darius.mercadier@gmail.com>
2019-10-27 22:51:41 +0100

    Switch to an AST model for TightPROVE

--- src/tightprove/parsing/parser_api_tp.ml
+++ src/tightprove/parsing/parser_api_tp.ml
@@ -8,12 +8,12 @@ let print_position outx lexbuf =
   fprintf outx "%s:%d:%d" pos.pos_fname
           pos.pos_lnum (pos.pos_cnum - pos.pos_bol + 1)
 
-let parse_file (filename:string) : Tp_AST.prog =
+let parse_file (filename:string) : Tp_AST.def =
   let f = open_in filename in
   let lex = from_channel f in
   try
     lex.lex_curr_p <- { lex.lex_curr_p with pos_fname = filename; };
-    Parser_tp.prog Lexer_tp.token lex
+    Parser_tp.def Lexer_tp.token lex
   with
   | SyntaxError msg ->
      fprintf stderr "%a: %s\n" print_position lex msg;
@@ -35,8 +35,8 @@ let parse_generic (parse_fun:(Lexing.lexbuf -> Parser_tp.token) -> Lexing.lexbuf
      fprintf stderr "%a: syntax error\n" print_position lex;
      exit (-1)
 
-let parse_prog (str:string) : Tp_AST.prog =
-  parse_generic Parser_tp.prog str
+let parse_def (str:string) : Tp_AST.def =
+  parse_generic Parser_tp.def str
 
 let parse_expr (str:string) : Tp_AST.expr =
   parse_generic Parser_tp.expr_a str
