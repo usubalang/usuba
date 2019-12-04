3085336c2131bb61d9ffe03803a7051dab58c586
DadaIsCrazy <darius.mercadier@gmail.com>
2019-12-04 11:38:45 +0100

    Add script to install dependencies

---
+++ install_deps.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Installing OCaml
+if [ ! $(command -v ocaml) ]; then
+    echo "OCaml not found. Installing it...";
+    sudo apt install -y ocaml
+fi
+
+# Installing and configuring Opam (OCaml package manager)
+if [ ! $(command -v opam) ]; then
+    echo "Opam not found. Installing it...";
+    sudo apt install -y opam
+    opam init -y
+    eval `opam config env`
+fi
+
+# Installing required packages from opam
+opam install -y coq ocamlbuild menhir
