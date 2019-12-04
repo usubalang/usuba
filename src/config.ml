6c83432b39d42880838a4a9f172ecc142246fab6
DadaIsCrazy <darius.mercadier@gmail.com>
2019-12-04 11:28:42 +0100

    Update configuration file

--- src/config.ml
+++
@@ -1,6 +0,0 @@
-(* This file was automatically generated. Manual edits might be overriten
- whenever the configure script is ran again. *)
-
-
-let data_dir = "/home/dada/Workspace/usuba/data"
-let tightprove_cache = "/home/dada/Workspace/usuba/tightprove_cache"
