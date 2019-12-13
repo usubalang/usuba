d9d688e1a5f2bb3b023c04933584db5caa9fc9b9
DadaIsCrazy <darius.mercadier@gmail.com>
2019-12-13 11:20:32 +0100

    Fix new paths for skiva

--- experimentations/check_aes_sbox/compile.pl
+++ experimentations/check_aes_sbox/compile.pl
@@ -11,13 +11,13 @@ for my $fd (1, 2, 4) {
     for my $ti (1, 2, 4) {
         say "Compiling FD=$fd TI=$ti";
         if ($ARGV[0] && $ARGV[0] eq "X86") {
-            system "gcc -I ../../arch -Wall -Wextra -Wno-unused-function -o main_${fd}_${ti} main.c sbox.c -D TI=$ti -D FD=$fd -D X86";
+            system "gcc -I ../../arch/FAME -Wall -Wextra -Wno-unused-function -o main_${fd}_${ti} main.c sbox.c -D TI=$ti -D FD=$fd -D X86";
             if ($run) {
                 say "Running it";
                 system "./main_${fd}_${ti}";
             }
         } else {
-            system "sparc-gaisler-elf-gcc -Wall -Wextra -Wno-unused-function -mcpu=leon3 -mno-fpu -Wall -Wextra -D TI=$ti -D FD=$fd -I ../../arch -O3 -o main_${fd}_${ti} main.c sbox.c";
+            system "sparc-gaisler-elf-gcc -Wall -Wextra -Wno-unused-function -mcpu=leon3 -mno-fpu -Wall -Wextra -D TI=$ti -D FD=$fd -I ../../arch/FAME -O3 -o main_${fd}_${ti} main.c sbox.c";
         }
     }
 }
