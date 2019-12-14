4a251add2f8fb8673a5a6fc33e459ace68ceb3d0
DadaIsCrazy <darius.mercadier@gmail.com>
2019-12-14 14:55:31 +0100

    Fix SKIVA backend with new pipelining

--- arch/FAME/FAME_rand.h
+++ arch/FAME/FAME_rand.h
@@ -29,11 +29,11 @@ static unsigned long timer_count_cycles() {
   count = lr->timer[0].counter;
   return lap;
 }
-static int state = 0x8e20a6e5;
+static volatile int state = 0;
 static int remaining = 32;
 static int get_rand() {
   if (remaining == 0) {
-    state     = timer_count_cycles();
+    timer_count_cycles();
     remaining = 32;
   }
   int bits_per_rand = 32 / FD;
@@ -41,7 +41,7 @@ static int get_rand() {
   bits_per_rand /= 2;
 #endif
   remaining -= bits_per_rand;
-  return state; // Not quite correct, should depend on TI, FD and PIPELINED
+  return state;
 }
 #define RAND() get_rand()
 
@@ -49,71 +49,81 @@ static int get_rand() {
 
 #else // #ifdef COPROC_RAND
 
+// INTERLEAVE (and UNINTERLEAVE) convert data to (and back from)
+// pipelined (ie Rt = 2) format. See explanations in aes.c somewhere.
+#ifdef PIPELINED
+#if FD == 1
+#define SHIFT 16
+#define MASK_RIGHT 0x0000ffff
+#define MASK_LEFT  0xffff0000
+#elif FD == 2
+#define SHIFT 8
+#define MASK_RIGHT 0x00ff00ff
+#define MASK_LEFT  0xff00ff00
+#elif FD == 4
+#define SHIFT 4
+#define MASK_RIGHT 0x0f0f0f0f
+#define MASK_LEFT  0xf0f0f0f0
+#endif // #if FD == 1
+
+#define INTERLEAVE(a,b) ((a & MASK_RIGHT) | ((b << SHIFT) & MASK_LEFT))
+#define UNINTERLEAVE(a) (a & MASK_RIGHT)
+
+#endif // #ifdef PIPELINED
+
+#if FD == 1
+#define FORMAT(x) x
+#elif FD == 2
+#define FORMAT(x) ((x & 0x0000ffff) | (~x << 16))
+#elif FD == 4
+#define FORMAT(x) ((x & 0x000000ff) | ((x & 0x000000ff) << 16) | \
+                   ((~x & 0x000000ff) << 8) | ((~x & 0x000000ff) << 24))
+#endif // #if FD == 1
+
 // There is no coprocessor to generate random bits -> using a xorshift
 // RNG (namely, one that Wikipedia
 // (https://en.wikipedia.org/wiki/Xorshift) attributes to p.4 of
 // Marsaglia, "Xorshift RNGs").
+
 static int state = 0x8e20a6e5;
+
+// In pipelined mode, we need to compute twice each round with the
+// same randomness. To do so, we need that |state_prev| as well as
+// |seed|, |seed_prev| and |get_seed| to reset the seed between rounds
+// as needed. See their usage in aes.c.
 #ifdef PIPELINED
 static int state_prev = 0x8e20a6e5;
-#endif
 static void seed(int seed) { state = seed; }
-#ifdef PIPELINED
 static int get_seed() { return state; }
 static void seed_prev(int seed) { state_prev = seed; }
 #endif
+
 static int __attribute__((noinline)) xorshift_rand() {
   state ^= state << 13;
   state ^= state >> 17;
   state ^= state << 5;
+  int state_R = FORMAT(state);
 #ifdef PIPELINED
   state_prev ^= state_prev << 13;
   state_prev ^= state_prev >> 17;
   state_prev ^= state_prev << 5;
+  int state_prev_R = FORMAT(state_prev);
 #endif
-#if FD == 1
+
 #ifdef PIPELINED
-  return (state_prev << 16) | (state & 0xFFFF);
+  return INTERLEAVE(state_R, state_prev_R);
 #else
-  return state;
-#endif
-#elif FD == 2
-  // Can't have both PIPELINED and FD == 2
-  return (~state << 16) | (state & 0xFFFF);
-#elif FD == 4
-#ifdef PIPELINED
-  return (~state_prev << 24) | ((state_prev & 0xFF) << 16) |
-    ((~state & 0xFF) << 8) | (state & 0xFF);
-#else
-  return (~state << 24) | ((state & 0xFF) << 16) |
-    ((~state & 0xFF) << 8) | (state & 0xFF);
-#endif
+  return state_R;
 #endif
 }
 
-#if defined(FD) && FD >= 2
-#if FD == 2
-#define RAND() ({ DATATYPE _tmp = xorshift_rand(); (~_tmp & 0xFFFF) | (_tmp << 16); })
-#elif FD == 4
-#define RAND() ({ DATATYPE _tmp = xorshift_rand();                      \
-      (~_tmp & 0xFF) | ((_tmp & 0xFF) << 8) | ((~_tmp & 0xFF) << 16) | (_tmp<< 24); })
-#else // #if FD == 2 ... #elif FD == 4
-#error Invalid FD
-#endif // #if FD == 2
-#else // #if defined(FD) && FD >= 2
 #define RAND() xorshift_rand()
-#endif // #if defined(FD) && FD >= 2
+
 #endif // #ifdef COPROC_RAND
 
 
 #ifdef CST_RAND
 #undef RAND
-#ifdef X86
 static volatile int x = 0;
 #define RAND() x
-#else // #ifdef X86
-//#define RAND() (unsigned int) *((volatile unsigned int*)0x80000600)
-static volatile int x = 0;
-#define RAND() x
-#endif // #ifdef X86
 #endif // #ifdef CST_RAND
