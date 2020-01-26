627b6c4e60e1550c87660a1140193b021dfcab4a
DadaIsCrazy <darius.mercadier@gmail.com>
2020-01-26 11:33:32 +0100

    [add expe] comment run script

--- experimentations/add/perfs/run.pl
+++ experimentations/add/perfs/run.pl
@@ -5,15 +5,15 @@ use warnings;
 use v5.14;
 use autodie qw( open close );
 
-my $source_file = 'add.s';
-
+# Print output headers
 say "| Padding |       Cycles      |   MITE uops   |   DSB uops    |" . 
     "    DSB miss   | DSB miss penalty |";
 say "| ------- | ----------------- | ------------- | ------------- |" . 
     " ------------- | ---------------- |";
 
 for my $n (0 .. 128) {
-    open my $FH_IN, '<', $source_file;
+    # Copy add.s to add some padding
+    open my $FH_IN, '<', 'add.s';
     open my $FH_OUT, '>', 'tmp.s';
     while (<$FH_IN>) {
         print $FH_OUT $_;
@@ -23,24 +23,28 @@ for my $n (0 .. 128) {
     }
     close $FH_OUT;
 
+    # Compile the new assembly file
     system "clang -o tmp tmp.s";
+
+    # Run perf
     my $perf_output = `perf stat -e idq.dsb_uops,idq.mite_uops,frontend_retired.dsb_miss,dsb2mite_switches.penalty_cycles,cycles ./tmp 2>&1`;
+
+    # Extract perf results from perf output
     my ($dsb_uops)       = $perf_output =~ /(\d+(?: \d+)*)\s*idq.dsb_uops/;
     my ($mite_uops)      = $perf_output =~ /(\d+(?: \d+)*)\s*idq.mite_uops/;
     my ($dsb_miss)       = $perf_output =~ /(\d+(?: \d+)*)\s*frontend_retired.dsb_miss/;
     my ($penalty_cycles) = $perf_output =~ /(\d+(?: \d+)*)\s*dsb2mite_switches.penalty_cycles/;
     my ($cycles)         = $perf_output =~ /(\d+(?: \d+)*)\s*cycles/;
 
-    my $start_alignment = $n;
-    my $jump_alignment = $n+16;
-
+    # Format number of cycles so that outliers are in bold
     if (($cycles =~ s/ //gr) < 2_400_000_000 || 
         ($cycles =~ s/ //gr) > 2_600_000_000) {
         $cycles = "**" . $cycles . "**";
     } else {
         $cycles = "  " . $cycles . "  ";
     }
-    
+
+    # Print the results for this padding
     printf "| %7d | %13s | %13s | %13s | %13s | %16s |\n",
       $n, $cycles, $mite_uops, $dsb_uops, $dsb_miss, $penalty_cycles
 }
