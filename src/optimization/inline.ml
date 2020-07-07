9d03b5dbac6ca5a936b565683ab261b69eb94e88
DadaIsCrazy <darius.mercadier@gmail.com>
2020-07-07 19:26:34 +0200

    Fix inlining bug
    Nodes marked _inline were not always inlined.

--- src/optimization/inline.ml
+++ src/optimization/inline.ml
@@ -118,7 +118,8 @@ and can_inline env inlined conf (node:def) : bool =
   else if is_call_free env inlined conf node then
     (* Node doesn't contain any function call that should be inlined
        -> heuristically deciding to inline it or not *)
-    (not (is_noinline node)) && (should_inline_heuristic node)
+    (is_inline node) ||
+      ((not (is_noinline node)) && (should_inline_heuristic node))
   else
     false
 
