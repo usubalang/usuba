84ef25834f1c084f2c52f6c5529a338287412987
mattiasdrp <5543639+mattiasdrp@users.noreply.github.com>
2022-04-08 12:09:47 +0200

    ocamlformat 0.21.0

--- src/tightprove/tp_AST.ml
+++ src/tightprove/tp_AST.ml
@@ -7,7 +7,8 @@ type expr =
   | Const of int
   | ConstAll of int
   | Refresh of string
-  | BitToReg of string * int (* useful for Pyjamask: -1 if string.int == 1; 0 otherwise *)
+  | BitToReg of
+      string * int (* useful for Pyjamask: -1 if string.int == 1; 0 otherwise *)
   | Not of string
   | Log of log_op * string * string
   | Shift of shift_op * string * int
