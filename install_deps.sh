52bf213a9be596562ef01e3f9e172b392b11f05b
Pierre-Evariste Dagand <doc@evr.ist>
2022-09-08 15:55:33 +0200

    Dependency management: leave it to `opam build`

--- install_deps.sh
+++ install_deps.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Installing OCaml
-if [ ! $(command -v ocaml) ]; then
-    echo "OCaml not found. Installing it...";
-    sudo apt install -y ocaml
-fi
-
 # Installing and configuring Opam (OCaml package manager)
 if [ ! $(command -v opam) ]; then
     echo "Opam not found. Installing it...";
@@ -15,7 +9,4 @@ if [ ! $(command -v opam) ]; then
 fi
 
 # Installing required packages from opam
-opam install -y dune menhir
-
-# Installing required modules for Perl scripts
-sudo cpanm Crypt::Mode::ECB
+opam -y build
